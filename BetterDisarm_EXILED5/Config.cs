a55fbe18d5287fad82ec2b35a045b63a6ca24d0d
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-02-17 16:22:31 +0100

    Add files via upload

---
+++ BetterDisarm_EXILED5/Config.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace BetterDisarm
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        [Description("Hint that will appear to the attacker when it tried to hurt cuffed people")]
+        public string AttackerHint { get; set; } = "You cant hurt cuffed people";
+        public float AttackerHintDuration { get; set; } = 5;
+    }
+}
---
+++ BetterDisarm_EXILED5/EventHandlers.cs
@@ -0,0 +1,22 @@
+﻿using Exiled.Events.EventArgs;
+
+namespace BetterDisarm
+{
+    public class EventHandlers
+    {
+        public readonly MainClass plugin;
+        public EventHandlers(MainClass plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnHurting(HurtingEventArgs ev)
+        {
+            if (ev.Target.IsCuffed & ev.Attacker.IsHuman)
+            {
+                ev.IsAllowed = false;
+                ev.Attacker.ShowHint(plugin.Config.AttackerHint, plugin.Config.AttackerHintDuration);
+            }
+        }
+    }
+}
